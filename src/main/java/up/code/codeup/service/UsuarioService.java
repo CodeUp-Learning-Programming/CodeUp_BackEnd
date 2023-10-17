483c5b56e635081450fd92922b639a503048e600
Yah-olih <yara.santana@sptech.school>
2023-10-17 10:00:17 -0300

    csv

--- src/main/java/up/code/codeup/service/UsuarioService.java
+++ src/main/java/up/code/codeup/service/UsuarioService.java
@@ -1,5 +1,10 @@
 package up.code.codeup.service;
 
+import com.opencsv.*;
+import com.opencsv.exceptions.CsvValidationException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,6 +12,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import up.code.codeup.configuration.security.jwt.GerenciadorTokenJwt;
 import up.code.codeup.dto.usuarioDto.UsuarioCriacaoDto;
@@ -16,8 +23,10 @@ import up.code.codeup.entity.Usuario;
 import up.code.codeup.mapper.UsuarioMapper;
 import up.code.codeup.repository.UsuarioRepository;
 
+import java.io.*;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 @Service
 public class UsuarioService {
@@ -88,4 +97,80 @@ public class UsuarioService {
     }
 
 
-}
+    public void gravaUsuariosEmArquivoCsv(List<Usuario> usuarios, String nomeArq) {
+        nomeArq += ".csv";
+
+        try (FileWriter arquivoCsv = new FileWriter(nomeArq)) {
+            for (Usuario usuario : usuarios) {
+                String linha = String.format("%d;%s;%s;%s;%s\n",
+                        usuario.getIdUsuario(),
+                        usuario.getNome(),
+                        usuario.getEmail(),
+                        usuario.getSenha(),
+                        usuario.getDtNascimento());
+
+                arquivoCsv.write(linha);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void lerExibirArquivoCsv(String nomeArq) {
+        nomeArq += ".csv";
+
+        try (FileReader arq = new FileReader(nomeArq);
+             Scanner entrada = new Scanner(arq).useDelimiter(";|\\n")) {
+
+            System.out.printf("%-4s %-15s %-17s %-60s %-15s\n", "ID", "NOME", "EMAIL", "SENHA", "DTNASCIMENTO");
+            while (entrada.hasNext()) {
+                try {
+                    int id = Integer.parseInt(entrada.next());
+                    String nome = entrada.next();
+                    String email = entrada.next();
+                    String senha = entrada.next();
+                    String dtNascimento = entrada.next();
+                    System.out.printf("%04d %-15s %-17s %-60s %-15s\n", id, nome, email, senha, dtNascimento);
+                } catch (NumberFormatException e) {
+                    System.err.println("Erro ao converter um valor para inteiro. Ignorando a linha.");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void saveUsuariosFromCsv(MultipartFile file) {
+        try {
+            CSVParser csvParser = new CSVParserBuilder()
+                    .withSeparator(';') // Configura o delimitador como ponto e vírgula
+                    .build();
+
+            CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()));
+
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                Usuario usuario = new Usuario();
+                usuario.setIdUsuario(Integer.parseInt(line[0]));
+                usuario.setNome(line[1]);
+                usuario.setEmail(line[2]);
+                usuario.setSenha(line[3]);
+                // Parse LocalDate from line[4] if needed
+
+                // Save the Usuario entity to the H2 Database
+                entityManager.persist(usuario);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+        }
