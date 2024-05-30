cab48b4a5b82e4fb20a072d7a9c1a03b45332058
M4theusRibeiro <matheusribeiro3105@gmail.com>
2024-05-30 02:21:27 -0300

    Resolvendo UTF-8 4

--- src/main/java/up/code/codeup/CodeupApplication.java
+++ src/main/java/up/code/codeup/CodeupApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodeupApplication {
+// ##########################################
+// ATENÇÃO, NÃO APAGAR ESSE COMENTÁRIO
+// ##########################################
+
 
     public static void main(String[] args) {
         SpringApplication.run(CodeupApplication.class, args);
