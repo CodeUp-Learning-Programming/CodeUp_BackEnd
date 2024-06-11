e758086ac76777738cefbc7f0bed1092d196dcb6
Rostichelli <douglas.alves@sptech.school>
2024-06-11 11:42:42 -0300

    Fix comprar/{idItem}

--- src/main/java/up/code/codeup/service/LojaService.java
+++ src/main/java/up/code/codeup/service/LojaService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import up.code.codeup.dto.usuarioDto.UsuarioAtualizado;
 import up.code.codeup.entity.ItemAdquirido;
 import up.code.codeup.entity.ItemLoja;
 import up.code.codeup.entity.Usuario;
@@ -13,6 +12,7 @@ import up.code.codeup.repository.ItemLojaRepository;
 import up.code.codeup.repository.UsuarioRepository;
 import up.code.codeup.utils.UsuarioUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,11 +33,13 @@ public class LojaService {
         if (usuario.getMoedas() < item.getPreco()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Moedas insuficientes");
         }
+
         usuario.getItemAdquiridos().forEach(itemAdquirido -> {
             if (itemAdquirido.getItemLoja().getId() == idItem) {
                 throw new ResponseStatusException(HttpStatus.CONFLICT, "Item já adquirido");
             }
         });
+
         ItemAdquirido itemAdquirido = new ItemAdquirido();
         itemAdquirido.setItemLoja(item);
         itemAdquirido.setUsuario(usuario);
@@ -45,6 +47,9 @@ public class LojaService {
         usuario.setMoedas(usuario.getMoedas() - item.getPreco());
         usuarioRepository.save(usuario);
         itemAdquiridoRepository.save(itemAdquirido);
+        List<ItemAdquirido> newList = usuario.getItemAdquiridos();
+        newList.add(itemAdquirido);
+        usuario.setItemAdquiridos(newList);
         return usuario;
     }
 }
